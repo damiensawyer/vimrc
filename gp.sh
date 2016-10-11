0af8715aef0dac4ec1f80c76dbe9ee8990eab514
Damien Sawyer <damiensawyer@DEV-iMac.local>
2016-10-12 10:17:13 +1100

    param check

--- gp.sh
+++ gp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # quick way to commit all changes
 if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters"
+    echo "Illegal number of parameters. Pass only commit message. Use double quotes if multiple words"
     exit 1
 fi
 git add -A && git ci -m "$1"
